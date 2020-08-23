01a4c76fd4239247fea1dc7109ba9b1c779789c0
Duc Thanh Nguyen <thanhnguyenduc157@gmail.com>
2020-08-23 23:35:20 +0700

    Revert "first commit"
    This reverts commit 8f6c4bef2d479814202e35f823b39fe88e5aed89.

--- android/app/src/main/kotlin/com/collect/beans/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.collect.beans
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/value/styles.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-class Styles {
-  static TextStyle textStyleSmall = GoogleFonts.montserrat(
-      fontSize: 9, fontWeight: FontWeight.w400);
-  static  TextStyle textStyleMedium = GoogleFonts.montserrat(
-    fontSize: 17, color: Colors.deepPurple[500],  fontWeight: FontWeight.w500);
-}
