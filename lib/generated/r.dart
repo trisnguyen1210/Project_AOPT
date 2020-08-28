9771b4e5f66bfc30b65a4a5072daffc8720fa507
Duc Thanh Nguyen <thanhnguyenduc157@gmail.com>
2020-08-28 09:18:01 +0700

    update file R (generated)

---
+++ lib/generated/r.dart
@@ -0,0 +1,13 @@
+class R {
+  static const String ic_bean = "assets/ic_bean.svg";
+  static const String ic_god = "assets/ic_god.png";
+  static const String ic_other_guy_svg = "assets/ic_other_guy.svg";
+  static const String ic_myself = "assets/ic_myself.png";
+  static const String ic_home = "assets/ic_home.svg";
+  static const String ic_myself_svg = "assets/ic_myself.svg";
+  static const String ic_calendar = "assets/ic_calendar.svg";
+  static const String tooltip = "assets/tooltip.png";
+  static const String ic_god_svg = "assets/ic_god.svg";
+  static const String ic_confession = "assets/ic_confession.svg";
+  static const String ic_other_guys = "assets/ic_other_guys.png";
+}
